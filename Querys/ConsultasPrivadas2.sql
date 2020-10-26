79b3520efef6564f2d93c00fca408b458b13ca49
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-10-25 22:56:38 -0600

    Creacion tabla aseguradora, paciente y urgencia

--- Querys/ConsultasPrivadas2.sql
+++ Querys/ConsultasPrivadas2.sql
@@ -1,2 +1,33 @@
 CREATE DATABASE ConsultasPrivadas2;
 USE ConsultasPrivadas2;
+/*Creacion tabla Seguro*/
+CREATE TABLE aseguradora(
+	id_aseguradora int IDENTITY (1,1),
+	nombre_aseguradora varchar(45),
+	domicilio_social_aseguradora varchar(100),
+	CIF_aseguradora varchar(45),
+	CONSTRAINT PK_aseguradora PRIMARY KEY (id_aseguradora)
+);
+/*Creacion tabla paciente*/
+CREATE TABLE paciente(
+	id_paciente int IDENTITY (1,1),
+	nombre_paciente varchar(45),
+	apellido_paciente varchar(45),
+	domicilio_paciente varchar(100),
+	sexo_paciente varchar(12),
+	fecha_naciemiento_paciente varchar(45),
+	id_aseguradora int,
+	CONSTRAINT PK_paciente PRIMARY KEY (id_paciente),
+	CONSTRAINT FK_aseguradoraP FOREIGN KEY (id_paciente) REFERENCES dbo.paciente (id_paciente)
+);
+/*Creacion tabla Urgencia*/
+CREATE TABLE urgencia(
+	id_urgencia int IDENTITY (1,1),
+	nombre_urgencia varchar(45),
+	apellido_urgencia varchar(45),
+	fecha_hora_urgencia varchar(45),
+	descripcion_urgencia varchar(100),
+	id_paciente int,
+	CONSTRAINT PK_urgencia PRIMARY KEY (id_urgencia),
+	CONSTRAINT FK_pacienteU FOREIGN KEY (id_urgencia) REFERENCES dbo.urgencia (id_urgencia)
+);
